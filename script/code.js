84b7c0b76d250336b7328130c5505bac9802d6b0
RayhaanK <rayhaankhan331@gmail.com>
2023-05-26 11:56:33 +0200

    added task

---
+++ script/code.js
@@ -0,0 +1,14 @@
+function greet(){
+    console.log('Hello');
+}
+greet()
+function multiply(a, b){
+    console.log(a*b);
+}
+multiply(2, 5);
+// function tableOfFive(x)
+let y = tableOfFive(5);
+function tableOfFive(y){
+    return y * 5;
+}
+console.log(y * 5)
